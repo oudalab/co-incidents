331e31b79e9ab8cef5c393f69d1f7954fe494636
YanLiang <yanliang1102@gmail.com>
2018-11-04 22:58:24 -0800

    run the cross year experiment

---
+++ sourceCode/runcross.sh
@@ -0,0 +1,22 @@
+
+#!/bin/bash
+./cr 12 1000 test1975 test1976 &
+./cr 12 1000 test1977 test1978 &
+./cr 12 1000 test1979 test1980 &
+./cr 12 1000 test1981 test1982 &
+./cr 12 1000 test1983 test1984 &
+./cr 12 1000 test1985 test1986 &
+./cr 12 1000 test1987 test1988 &
+./cr 12 1000 test1989 test1990 &
+./cr 12 1000 test1991 test1992 &
+./cr 12 1000 test1993 test1994 &
+./cr 12 1000 test1995 test1996 &
+./cr 12 1000 test1997 test1998 &
+./cr 12 1000 test1999 test2000 &
+./cr 12 1000 test2001 test2002 &
+./cr 12 1000 test2003 test2004 &
+./cr 12 1000 test2005 test2006 &
+./cr 12 1000 test2007 test2008 &
+./cr 12 1000 test2009 test2010 &
+./cr 12 1000 test2011 test2012 &
+./cr 12 1000 test2013 test2014 &
