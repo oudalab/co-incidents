bfde3a342ecb9b3eab263c7122df35b2f57b962a
YanLiang <yanliang1102@gmail.com>
2019-02-05 22:47:51 -0800

    Update co_yan.cpp

--- code/co_yan.cpp
+++ code/co_yan.cpp
@@ -32,30 +32,6 @@ using namespace std;
 
 int lastActiveIncidenceIndex = 0;
 
-//to track if the feature turned on or off on each incidence, if incidence feature has a value it means it is turned on, otherwise it means it is turned off.
-class IncidenceFeature
-{
-public:
-    map<string, string> featureMap;
-    IncidenceFeature()
-    {
-        //all the feature should be turned on at the beginning
-        //all the names here are the same as the json file, if it has value means it is turned on, if it is "" string, means this feature is turned off
-        featureMap["code"] = "";
-        featureMap["country_code"] = "";
-        featureMap["date8"] = "";
-        featureMap["geoname"] = "";
-        featureMap["id"] = "";
-        featureMap["year"] = "";
-        featureMap["latitude"] = "";
-        featureMap["longitude"] = "";
-        featureMap["src_actor"] = "";
-        featureMap["src_agent"] = "";
-        featureMap["tgt_actor"] = "";
-        featureMap["tgt_agent"] = "";
-    }
-};
-
 class SharedResources
 {
 
